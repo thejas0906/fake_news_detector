d67743178ee8f4797a418975392acc8cd6b20d23
thejas0906 <kickgunther4@gmail.com>
2026-02-26 21:38:24 +0530

    Login Backend added

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,20 @@
 
 from backend.models import User,News,Token,TokenData
 from fastapi import FastAPI,Depends,HTTPException,status,APIRouter
-from backend.database import session,engine,get_db
+from backend.database import engine,get_db
 from typing import Annotated
 import backend.models_db as models_db
 from sqlalchemy.orm import Session
-from dotenv import load_dotenv,dotenv_values
+from dotenv import load_dotenv
 from pwdlib import PasswordHash
 from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
 from fastapi_swagger_dark import install
 import fastapi_swagger_dark as fsd
-import jwt
-import os,uuid
-from jwt.exceptions import InvalidTokenError
-from datetime import datetime, timedelta, timezone
+import os
+from datetime import timedelta
 from backend.functions.auth_functions import get_current_user,authenticate,create_access_token,oauth2_scheme,password_hash
 load_dotenv() # laoding the env details from .env
+
 app = FastAPI(docs_url=None)
 router = APIRouter()
 fsd.install(router)
@@ -74,7 +73,6 @@ def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,Depend
     access_token_expires=timedelta(minutes=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")))
     access_token=create_access_token(data={"sub":str(auth_user.user_id)},expires_delta=access_token_expires)
     return Token(access_token=access_token, token_type="bearer")
-
 
 
 
